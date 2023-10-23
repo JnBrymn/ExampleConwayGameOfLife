4c9e3962390def4525fc8eb6aa517cccb99c9338
John Berryman <johnberryman@Johns-MBP.tds.net>
2023-10-23 12:49:09 -0500

    commit number 2

--- explain_turn.go
+++ explain_turn.go
@@ -64,7 +64,7 @@ func NewTurn(bt *base.Turn) (*Turn, error) {
 // Prompt generates a prompt for a Turn.
 func (t *Turn) Prompt(ctx context.Context) ([]*conversation.Message, error) {
 	m, err := t.pb.Build()
-	if err != nil {
+	if err != nil { // TODO: make this better
 		return nil, err
 	}
 
